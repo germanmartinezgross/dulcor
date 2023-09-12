98169dafbdf3712830979918ffe4d2146900f12c
German <germanmartinezgross@outlook.com>
2023-09-12 14:57:58 -0300

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["estadoCuenta/dulcor/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"estadoCuenta/dulcor/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
